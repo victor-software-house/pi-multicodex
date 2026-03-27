f929e557fb67c9ac6e73d92f2c0bcec235333331
Victor Araújo <victor@victor-software-house.com>
2026-03-27 20:26:01 -0300

    build: explicit semver rules and conventionalcommits preset

--- release.config.mjs
+++ release.config.mjs
@@ -1,7 +1,19 @@
 export default {
 	branches: ["main"],
 	plugins: [
-		"@semantic-release/commit-analyzer",
+		[
+			"@semantic-release/commit-analyzer",
+			{
+				preset: "conventionalcommits",
+				releaseRules: [
+					{ breaking: true, release: "major" },
+					{ type: "feat", release: "minor" },
+					{ type: "fix", release: "patch" },
+					{ type: "perf", release: "patch" },
+					{ type: "revert", release: "patch" },
+				],
+			},
+		],
 		"@semantic-release/release-notes-generator",
 		[
 			"@semantic-release/changelog",
@@ -28,7 +40,7 @@ export default {
 			"@semantic-release/git",
 			{
 				assets: ["package.json", "pnpm-lock.yaml", "CHANGELOG.md"],
-				message: String.raw`release: \${nextRelease.version}
+				message: String.raw`chore(release): \${nextRelease.version} [skip ci]
 
 \${nextRelease.notes}`,
 			},
